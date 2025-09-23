a6e27ea83b295c104167711beaa0a830fabd3b05
Blurry_Rain <miguelmikeswazy@gmail.com>
2025-09-23 17:01:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-async function fetchData(){
-
-    try{
-
+async function fetchData() {
+    try {
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-        if(!response.ok){
+        const card= document.getElementById("card");
+        if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
 
         const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
-
-        imgElement.src = pokemonSprite;
+        imgElement.src = data.sprites.front_default;
+        card.style.display="block";
         imgElement.style.display = "block";
-    }
-    catch(error){
+
+      
+        const typeEl = document.getElementById("type");
+        const abilityEl = document.getElementById("ability");
+        const movesEl = document.getElementById("moves");
+
+       
+        const types = data.types.map(t => t.type.name).join(", ");
+        typeEl.textContent = "Type: " + types;
+
+        const abilities = data.abilities.map(a => a.ability.name).join(", ");
+        abilityEl.textContent = "Abilities: " + abilities;
+
+       
+        const moves = data.moves.slice(0, 5).map(m => m.move.name).join(", ");
+        movesEl.textContent = "Moves: " + moves;
+
+    } catch (error) {
         console.error(error);
     }
 }
